ed831cf0b04d999ca3ab9791639a883e02293f55
Gustavo Zucolotto <guguzucoloto@gmail.com>
2020-06-15 17:42:34 -0300

    ordenação por nome adicionada

--- Unit1.pas
+++ Unit1.pas
@@ -179,6 +179,7 @@ var
 begin
   if not ADOQuery.IsEmpty then
   begin
+    ADOQuery.Sort := 'NOME';
     ADOQuery.First;
 
     EditListagem.Lines.Add('Código' + ' - ' + 'Nome' + ' - ' + 'Média' + ' - ' +
